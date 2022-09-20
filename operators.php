61b81902b43d71921b45a058528e76ebaa7e2dfc
gravityboys <alvix1337@wp.pl>
2022-09-20 10:25:56 +0200

    sdaf

---
+++ operators.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <title>Operatorzy</title>
+</head>
+<body>
+<a href="index.php">Strona główna</a>
+<br>
+<?php $x = 5;
+?>
+<?php $y = 15;
+?>
+<?php print $x + $y;
+?>
+<br>
+<?php print $x - $y;
+?>
+<br>
+<?php print $x * $y;
+?>
+<br>
+<?php print $x / $y;
+?>
